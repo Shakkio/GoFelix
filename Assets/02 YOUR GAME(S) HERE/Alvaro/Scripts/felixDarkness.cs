f4f005bec459ab04a2631e9a7c796588f21239d3
Barbarrok <alvarolinadi@gmail.com>
2023-11-24 15:00:41 +0100

    ª
    ª

--- Assets/02 YOUR GAME(S) HERE/Alvaro/Scripts/felixDarkness.cs
+++ Assets/02 YOUR GAME(S) HERE/Alvaro/Scripts/felixDarkness.cs
@@ -4,67 +4,62 @@ using UnityEngine;
 
 public class felixDarkness : MonoBehaviour
 {
-    public float spotlightRadius = 5f;
-    public LayerMask discoveredLayer; 
-    public Material revealedFelix; 
+    public GameObject victoryScreen;
+    public AudioClip clickSound;
+    public float gameDuration = 7f;
 
     public SpriteRenderer flashLightRenderer;
 
-    public GameObject gameOverImage; 
-    public float timeLimit = 7f; 
+    private bool hasWon = false;
+    private float timer = 0f;
 
-    private float currentTime;
-    private bool gameActive = true;
-
-    void Start()
-    {
-        currentTime = timeLimit;
-        gameOverImage.SetActive(false);
-        UpdateUI();
-    }
-
-    void Update()
-    {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        flashLightRenderer.transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            DiscoverObjects();
-        }
-
-        currentTime -= Time.deltaTime;
-
-        if (currentTime <= 0f)
-        {
-            GoFelixManager.Instance.win = false;
-        }
-
-        UpdateUI();
-    }
-
-    void UpdateUI()
+    private void Start()
     {
         
     }
 
-    void DiscoverObjects()
+    private void Update()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, spotlightRadius, discoveredLayer);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        flashLightRenderer.transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
 
-        foreach (Collider2D col in colliders)
+        timer += Time.deltaTime;
+
+        if (timer >= gameDuration && !hasWon)
         {
-            SpriteRenderer spriteRenderer = col.GetComponent<SpriteRenderer>();
+            LoseGame();
+        }
 
-            if (spriteRenderer != null)
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
             {
-                print ("You found me!");
-                GoFelixManager.Instance.win = true;
-            }
-            else
-            {
-                GoFelixManager.Instance.win = false;
+                if (hit.collider.CompareTag("Victory"))
+                {
+                    WinGame();
+                }
+                else if (hit.collider.CompareTag("Clickable"))
+                {
+                    AudioSource.PlayClipAtPoint(clickSound, hit.transform.position);
+                }
             }
         }
+    }
+
+    private void WinGame()
+    {
+        print("I won!");
+        hasWon = true;
+        victoryScreen.SetActive(true);
+        GoFelixManager.Instance.win = true;
+    }
+
+    private void LoseGame()
+    {
+        print("I lost!");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        //GoFelixManager.Instance.win = false;
     }
 }
